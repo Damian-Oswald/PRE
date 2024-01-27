d140809ea49c59dd7fa1266904c80a85ab66227d
Damian-Oswald <damian.oswald@protonmail.com>
2024-01-27 10:57:37 +0100

    Add proper description of end members
    Added both description as well as references for the end members help page.

--- R/getEpsilons.R
+++ R/getEpsilons.R
@@ -1,13 +1,13 @@
 #' @title Get the isotope end members
 #' 
-#' @param SPnit Site preference nitrification \insertCite{lewicka2017quantifying}{PRE}.
-#' @param SPden Site preference denitrification \insertCite{lewicka2017quantifying}{PRE}.
-#' @param eSPdiffusion Well et al. 2008 for eSPdiffusion.
-#' @param eSPred Lewicka-Szceback for eSPred.
-#' @param d18Onit Lewicka-Szceback for d18Onit.
-#' @param d18Oden Lewicka-Szceback for d18Oden.
-#' @param e18Odiffusion e18Odiffusion \insertCite{well2008isotope}{PRE}.
-#' @param e18Ored Lewicka-Szceback e18Ored.
+#' @param eta_SP_diffusion End member for diffusion of site-preference \insertCite{well2008isotope}{PRE}.
+#' @param eta_18O_diffusion End member for diffusion of `d18O` \insertCite{well2008isotope}{PRE}.
+#' @param SP_nitrification Site-preference for nitrification \insertCite{decock2013potential}{PRE}.
+#' @param d18O_nitrification `d18O` for nitrification \insertCite{lewicka2017quantifying}{PRE}.
+#' @param SP_denitrification Site-preference for denitrification \insertCite{decock2013potential}{PRE}.
+#' @param d18O_denitrification `d18O` for denitrification \insertCite{lewicka2017quantifying}{PRE}.
+#' @param eta_SP_reduction End member for SP reduction \insertCite{denk2017nitrogen}{PRE}.
+#' @param eta_18O_reduction End member for `d18O` reduction \insertCite{lewicka2017quantifying}{PRE}.
 #' 
 #' @returns A list of the eight isotope end members analogous to the function arguments.
 #' 
