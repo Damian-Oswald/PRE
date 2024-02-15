4c1362623c1f4b9e39d9b19a43fe7dc21073be88
Damian-Oswald <damian.oswald@protonmail.com>
2024-02-15 21:08:11 +0100

    Work on #5
    - Add description for `getIndices` function

--- R/getIndices.R
+++ R/getIndices.R
@@ -1,7 +1,11 @@
 #' @title Get the indices related to an index
 #' 
-#' @param data A data frame such as [`measurements`] with the following variables: `date`, `column`, and `depth`.
-#' @param i The index of the row of interest.
+#' @description
+#' In the various computations implemented in the process rate estimator, the task of finding the indices related to the measurements above and below a certain measurement comes up repeatedly. This task is automated with this function. It returns the indices of the top `i_top` and bottom `i_bottom` measurements.
+#' 
+#' @param data A data frame such as [`measurements`] with at least the following variables: `date`, `column`, and `depth`.
+#' @param i The index of the row of interest (in the data frame `data`).
+#' @param parameters A list of parameters as it's returned by the [`getParameters`] function.
 #' 
 #' @export
 getIndices <- function(data, i, parameters) {
